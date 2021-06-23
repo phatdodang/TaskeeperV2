aedc9f9c1c747084a905042ccd9a5b60738ef450
Phat Do <dodangphat99@gmail.com>
2021-06-23 15:40:43 +0700

    update project structure

---
+++ src/theme/StyleCommon.js
@@ -0,0 +1,9 @@
+import {
+    StyleSheet
+} from 'react-native';
+//file chứa các style sheet chung dành cho toàn components
+
+const StyleCommon = StyleSheet.create({
+    
+});
+export default StyleCommon;
---
+++ src/theme/index.js
@@ -0,0 +1,8 @@
+import StyleCommon from './StyleCommon';
+
+//file tổng hợp tất cả các style sheet trong theme để sử dụng
+const AppStyle = {
+    StyleCommon,
+    StyleMain,
+}
+export default AppStyle;
