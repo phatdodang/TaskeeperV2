51090eb459b474ecce35987687d3409dab206242
minhnhacmu2308 <65648352+minhnhacmu2308@users.noreply.github.com>
2021-07-01 23:37:38 +0700

    new

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,6 +8,6 @@ export interface Data {
 
 export const getLanguage = async () => {
     const response = await axios.get("https://taskeeperv2.herokuapp.com/codetables/isdcode?languageCode=en_US");
-    const result = response.data.data as Data;
+    const result = response.data.data as Data ;
     return result
 }
